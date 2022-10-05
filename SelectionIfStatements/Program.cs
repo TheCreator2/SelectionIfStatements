582c248781a45f31474b751a4aece5160706e229
Samuel Budge <17BudgeS@fulford.york.sch.uk>
2022-10-05 09:48:58 +0100

    Add project files.

---
+++ SelectionIfStatements/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SelectionIfStatements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a pizza flavour");
+            string choice = Console.ReadLine();
+            List<string> toppings = new List<string>()
+            {
+                "meat feast", "pepperoni", "cheese", "chickin", ""
+            };
+
+
+            if (20 > 18)
+            {
+                Console.WriteLine("20 is greater than 18");
+            }
+            Console.ReadLine();
+        }
+    }
+}
